8906bb385734ca0c1a3cfc7acfd41932b5204d0a
SrIsaac <isaac.silva1478@gmail.com>
2023-11-13 21:59:11 -0300

    class dto created

---
+++ src/main/java/com/cadastro/pessoas/dto/PersonRecordDto.java
@@ -0,0 +1,6 @@
+package com.cadastro.pessoas.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record PersonRecordDto(@NotBlank String name,@NotBlank int age, @NotBlank double saldo) {
+}
