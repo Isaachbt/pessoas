bb5c4cc269e44234be961844f193449ee3650b03
SrIsaac <isaac.silva1478@gmail.com>
2023-11-13 22:01:57 -0300

    class PersonRepository created

---
+++ src/main/java/com/cadastro/pessoas/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.cadastro.pessoas.repository;
+
+import com.cadastro.pessoas.model.PersonModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonModel, UUID> {
+
+}
