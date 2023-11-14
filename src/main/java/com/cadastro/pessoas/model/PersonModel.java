9100a5c37e9380c02ba2ed49ade703dbbb61261a
SrIsaac <isaac.silva1478@gmail.com>
2023-11-13 21:57:25 -0300

    class model created

---
+++ src/main/java/com/cadastro/pessoas/model/PersonModel.java
@@ -0,0 +1,31 @@
+package com.cadastro.pessoas.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_infor")
+public class PersonModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 3)
+    private int age;
+
+    @Column(nullable = false)
+    private double saldo;
+
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+}
