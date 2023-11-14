5058e80fb60fa85dadcf5d4ab1cd53f10919db22
SrIsaac <isaac.silva1478@gmail.com>
2023-11-13 21:51:26 -0300

    Initial commit

---
+++ src/main/java/com/cadastro/pessoas/CadastrandoPessoasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoPessoasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrandoPessoasSpringApplication.class, args);
+	}
+
+}
